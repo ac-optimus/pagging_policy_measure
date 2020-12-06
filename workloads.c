0bd4dafa48a1cdafc6fbb977e1efef8d8e43cacf
ac-optimus <abhavya.chandra@iitgn.ac.in>
2020-12-07 04:44:12 +0530

    removing unused files

--- workloads.c
+++ workloads.c
@@ -7,6 +7,7 @@
 
 #define NUM_OF_PAGE_TO_REFFERENCE 100000
 #define NUM_UNIQUE_PAGES 100
+// hash_size and NUM_UNIQUE_PAGES are the same
 void shuffle(int *array, size_t n){
     // taken from stack overflow -- https://stackoverflow.com/questions/6127503/shuffle-array-in-c
     if (n > 1)
@@ -22,8 +23,43 @@ void shuffle(int *array, size_t n){
     }
 }
 
+// void run_all_policies(int page_refferences[], int cache_size, int hash_size, FILE* fp, int size){
+//     int hits_random= 0, hits_fifo= 0, hits_lru= 0;
+//     int status;
+
+//     printf("------ running random --- \n");
+//     hashmap* cur_hash= create_hashmap(hash_size);
+//     queue* cur_queue= create_queue(cache_size);
+//     for (int page_no=0; page_no< 10000; page_no++){
+//         status= va_to_pa_RANDOM(cur_hash, cur_queue, page_refferences[page_no], cache_size, hash_size);
+//         if (status==1)
+//             hits_random+=1;
+//     }
+
+//     printf("------ running FIFO --- \n");
+//     hashmap* cur_hash_fifo= create_hashmap(hash_size);
+//     queue* cur_queue_fifo= create_queue(cache_size);
+//     for (int page_no=0; page_no<10000; page_no++){
+//         status= va_to_pa_FIFO(cur_hash_fifo, cur_queue_fifo, page_refferences[page_no]);
+//         if (status==1)
+//             hits_fifo+=1;
+//     }
+
+//     printf("------ running LRU --- \n");
+//     hashmap* cur_hash_lru= create_hashmap(hash_size);
+//     queue* cur_queue_lru= create_queue(cache_size);
+//     for (int page_no=0; page_no<10000; page_no++){
+//         status= va_to_pa_LRU(cur_hash_lru, cur_queue_lru, page_refferences[page_no]);
+//         if (status==1)
+//             hits_lru+=1;
+//     }
+//     fprintf(fp, "%d, %d, %d\n", hits_random, hits_lru, hits_fifo);
+// }
+
+
+
 void run_all_policies(int page_refferences[], int cache_size, int hash_size, FILE* fp, int size){
-    int hits_random= 0, hits_fifo= 0, hits_lru= 0;
+    int hits_random= 0, hits_fifo= 0, hits_lru= 0, hits_lru_approx= 0;
     int status;
 
     printf("------ running random --- \n");
@@ -52,8 +88,23 @@ void run_all_policies(int page_refferences[], int cache_size, int hash_size, FIL
         if (status==1)
             hits_lru+=1;
     }
-    fprintf(fp, "%d, %d, %d\n", hits_random, hits_lru, hits_fifo);
+
+    printf("------ running LRU APPROX --- \n");
+    hashmap* cur_hash_lru_approx= create_hashmap(hash_size);
+    queue* cur_queue_lru_approx= create_queue(cache_size);
+    // q_node clock_head;
+    int bits[hash_size];
+    for (int indx= 0; indx < hash_size; indx++)
+        bits[indx]=-1;
+    for (int page_no= 0; page_no < 10000; page_no++){
+        status= va_to_pa_LRU_APRROX(cur_hash_lru_approx, cur_queue_lru_approx, bits, page_refferences[page_no], cache_size, hash_size);
+        if (status==1)
+            hits_lru_approx+=1;
+    }
+    fprintf(fp, "%d, %d, %d, %d\n", hits_random, hits_lru, hits_fifo, hits_lru_approx);
 }
+
+
 
 void workload_random(int cache_size, int hash_size){
     FILE* fp;
